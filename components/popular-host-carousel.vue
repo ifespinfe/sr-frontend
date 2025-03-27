38a5bf7c05610506f94b829499e6ccc0b31e3695
seyiAjibola <info2mee.seyi@gmail.com>
2025-03-27 23:38:10 +0100

    audience dashboard refactoring

--- components/popular-host-carousel.vue
+++ components/popular-host-carousel.vue
@@ -4,7 +4,9 @@
       class="flex flex-row font-display text-2xl md:text-2xl font-semibold mb-5 mt-20 justify-between"
       >
         <div class="flex c">
-          <SvgIcon name="popular" /> Popular Hosts
+          <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" class="size-6 mt-1 me-2">
+              <path d="M0 24.68H24V0.68H0V24.68Z" fill="white"/>
+          </svg> Popular Hosts
         </div>
 
         <div class="order-last">
@@ -54,21 +56,11 @@
           <div class="text-muted-foreground mt-2">{{ item.followers }} Followers . {{ item.events }} Events</div>
           <div class="mt-4">
             <button 
-              v-if="!item.is_following" 
               class="uppercase inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 border border-ring h-10 px-10"
-              @click="followHost(item.id)"
+              @click="toggleFollow(item.id, item.is_following)"
             >
-              FOLLOW
+              {{ !item.is_following ? 'FOLLOW' : 'UNFOLLOW'}}
             </button>
-
-            <button 
-              v-if="item.is_following" 
-              class="uppercase inline-flex items-center justify-center whitespace-nowrap rounded-full text-base font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 border border-ring h-10 px-10"
-              @click="unFollowHost(item.id)"
-            >
-              UNFOLLOW
-            </button>
-           
           </div>
         </div>
       </div>
@@ -80,10 +72,8 @@
 </template>
 
 <script lang="ts" setup>
-import Carousel from "./shared/carousel.vue";
-import type { EmblaCarouselType } from "embla-carousel";
 import Button from "./components/ui/button.vue";
-import SvgIcon from "./components/svg-icon.vue";
+import SvgIcon from "./svg-icon.vue";
 
 const props = defineProps<{ popularHost: { 
   id: number | string;
@@ -98,19 +88,20 @@ const {
   unFollowUser
 } = useFollowActions();
 
-const followHost = (id) => {
-  followUser(id);
-  for (let i = 0; i < props.popularHost.length; i++) {
-    props.popularHost[i].is_following = true;
+const toggleFollow = (id, status) => {
+  if(!status) {
+    followUser(id)
+    toggleIsFollowing(true)
+  } else { 
+    unFollowUser(id);
+    toggleIsFollowing(false)
   }
 };
 
-const unFollowHost = (id) => {
-  unFollowUser(id);
-  for (let i = 0; i < props.popularHost.length; i++) {
-    props.popularHost[i].is_following = false;
-  }
-};
+const toggleIsFollowing = (status) => {
+  for (let i = 0; i < props.popularHost.length; i++)
+    props.popularHost[i].is_following = status;
+}
 
 </script>
 
