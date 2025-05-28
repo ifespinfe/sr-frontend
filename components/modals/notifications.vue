bbe12db03c7976ae4254092edbe1f2ebc8d0461f
lawrdev <lawrdev@gmail.com>
2025-05-28 23:59:16 +0100

    refactor notifications

--- components/modals/notifications.vue
+++ components/modals/notifications.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="fixed inset-x-0 bottom-0 bg-black z-[10] bg-cover px-2 md:px-4"
+    class="fixed inset-x-0 bottom-0 bg-black z-[20] bg-cover px-2 md:px-4"
     style="height: calc(100dvh - 74px)"
   >
     <div class="mt-8 max-w-[1400px] mx-auto">
       <!-- content -->
       <div class="px-0 md:px-6 max-w-5xl" role="dialog" aria-modal="true">
         <!-- header -->
-        <div class="text-start mb-3 flex items-start">
+        <div class="mb-5 text-start flex items-start">
           <Button
             :variant="'ghost'"
             @click="($event) => $emit('close')"
@@ -19,24 +19,33 @@
         </div>
         <!-- notifications -->
         <div
-          class="bg-[#A799FF10] space-y-2.5 text-white rounded-lg p-4 max-h-[72vh] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-800 [&::-webkit-scrollbar-thumb]:bg-gray-600 [&::-webkit-scrollbar-thumb]:rounded-lg [&::-webkit-scrollbar-thumb:hover]:bg-gray-500"
+          class="bg-[#A799FF10] space-y-2.5 text-white rounded-lg px-4 max-h-[76vh] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-800 [&::-webkit-scrollbar-thumb]:bg-gray-600 [&::-webkit-scrollbar-thumb]:rounded-lg [&::-webkit-scrollbar-thumb:hover]:bg-gray-500"
           v-if="prepareNotifications.length"
         >
           <div
             v-for="item in prepareNotifications"
             :key="item.id"
-            :class="!item.read_at ? 'bg-[#38373A]' : 'bg-white/5'"
-            class="rounded-3 border px-2 py-3 rounded-lg flex items-center gap-3 justify-start"
+            :class="!item.read_at ? 'bg-[#FFFFFF1A]' : 'bg-[#FFFFFF0D]'"
+            class="rounded-3 border border-[#38373A] px-2 py-2 rounded-lg flex items-center gap-3 justify-start"
           >
             <!-- icon -->
             <div class="shrink-0 mb-auto">
-              <Button
-                :variant="'destructive'"
-                class="!rounded-full border !p-2"
-                style="background-color: #e6684033"
+              <div
+                class="!rounded-full !p-2"
+                :style="{
+                  backgroundColor: item.read_at
+                    ? '#FFFFFF0D'
+                    : item?.data?.icon === 'exclamation'
+                    ? '#E6684033'
+                    : item?.data?.icon === 'mic_2'
+                    ? '#FF99F11A'
+                    : '#FFEE991A',
+                }"
               >
-                <SvgIcon :name="item.data.icon" />
-              </Button>
+                <SvgIcon
+                  :name="item.data.icon + (item.read_at ? `_default` : ``)"
+                />
+              </div>
             </div>
 
             <div class="w-full">
@@ -46,7 +55,7 @@
                   {{ item?.data?.message }}
                 </h5>
                 <div
-                  class="pt-1 text-muted-foreground text-xs font-medium shrink-0"
+                  class="pt-1 text-muted-foreground text-sm font-normal shrink-0 flex gap-2 items-start"
                 >
                   <!-- <span class="hidden md:inline-block">
                     {{ useDateFormat(item.created_at, "DD MMM, hh:mmA") }}
@@ -111,26 +120,28 @@ import Button from "../ui/button.vue";
 import SvgIcon from "../svg-icon.vue";
 import { Loader, Edit2, ChevronRight, Inbox } from "lucide-vue-next";
 
-const props = defineProps<{
-  notifications: {
-    id: string | number;
+interface Notification {
+  id: string | number;
+  type: string;
+  notifiable_type: string;
+  notifiable_id: string | number;
+  data: {
+    message: string;
+    status: string;
     type: string;
-    notifiable_type: string;
-    notifiable_id: string | number;
-    data: {
-      message: string;
-      status: string;
-      type: string;
-      details: string;
-      refund_amount: string;
-      icon?: string;
-    };
-    read_at: string | null;
-    created_at: string;
-    updated_at: string;
-  }[];
+    details?: string;
+    refund_amount?: string;
+    icon?: "music_note" | "mic_2" | "exclamation" | "bell" | string;
+  };
+  read_at: string | null;
+  created_at: string;
+  updated_at: string;
+}
+const props = defineProps<{
+  notifications: Notification[];
 }>();
 
+console.log("wellllll", props.notifications);
 const prepareNotifications = computed(() => {
   for (let i = 0; i < props.notifications.length; i++) {
     if (props.notifications[i].data.status == "rejected")
